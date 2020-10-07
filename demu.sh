f78d8143a1c032d6e9c9bffffaed6f9cc86f7b6a
yu xiang <yux.uth@gmail.com>
2020-10-07 19:47:07 -0400

    Update demu.sh

--- demu.sh
+++ demu.sh
@@ -14,9 +14,10 @@ index=$3
 
 python modify_fq.py -r1 ${r2} -o ${r2}.trim_2bp.fq 
 
+# e 0.15 for the 8 bp, which allow 1 bp mismatch
 cutadapt -e 0.15 --no-indels -a file:${index} \
 -o {name}_L001_R2_001.fq.gz \
 -p {name}_L001_R1_001.fq.gz \
-${r2}.trim_2bp.fq ${r1} > {r2}_demutiplex_log 
+${r2}.trim_2bp.fq ${r1} > ${r2}_demutiplex_log 
 
 rm -f  ${r2}.trim_2bp.fq
